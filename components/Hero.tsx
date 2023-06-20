d7d60dbbfa117b326ea9b42bc7514e0ac32d9786
Hector <hector.mendoza@urvenue.com>
2023-06-20 12:09:34 -0600

    Initial Layout and Functionality

---
+++ components/Hero.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Image from 'next/image'
+import { CustomButton } from '@/components/'
+
+const Hero = () => {
+
+    const handleScroll = () => {};
+
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book or rent a car -- quickly and easily!
+            </h1>
+            <p className='hero__subtitle'>
+                Streamline your car rental experience with our effortless 
+                online booking system.
+            </p>
+
+            <CustomButton 
+                title="Explore Cars"
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image 
+                    src="/hero.png"
+                    fill
+                    className='object-contain'
+                    alt="Hero"
+                />
+            </div>
+            <div className='hero__image-overlay' />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import CustomButton from './CustomButton';
+import CustomFilter from './CustomFilter';
+import Footer from './Footer';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CardDetails from './CardDetails';
+
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard , CardDetails};
